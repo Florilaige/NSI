c47076ad0a0e74d66fb7893c84410510136d8785
Florilaige <80651992+Florilaige@users.noreply.github.com>
2021-09-30 17:18:45 +0200

    Add exercices 6 and 7

--- Pile/Exercices.py
+++ Pile/Exercices.py
@@ -16,20 +16,36 @@ def echange(p):
 
 #3
 def inverse(p):
-    if p.taille() >= 2:
-        pl1 = Pile()
-        pl2 = Pile()
-        while not p.est_vide():
-            x = p.depile()
-            pl1.empile(x)
-            pl2.empile(x)
-        while not pl2.est_vide():
-            p.empile(pl2.depile())
-        return pl1
+    p_temp = Pile()
+    np = Pile()
+
+    while not p.est_vide():
+        el = p.depile()
+        p_temp.empile(el)
+        np.empile(el)
+
+
+    while not p_temp.est_vide():
+        el = p_temp.depile()
+        p.empile(el)
+
+    return np
 
 #4
 def copie(p):
-    return(inverse(inverse(p)))
+    p_temp = Pile()
+
+    while not p.est_vide():
+        el = p.depile()
+        p_temp.empile(el)
+
+    np = Pile()
+    while not p_temp.est_vide():
+        el = p_temp.depile()
+        np.empile(el)
+        p.empile(el)
+
+    return np
 
 #5.1
 def fond(p):
@@ -58,29 +74,21 @@ def rotation(p):
 
 #6
 def test_parenthesage(x):
-    nparenthèses = 0
-    ncrochets = 0
-    naccolades = 0
-    for i in x:
-        if i == "(":
-            nparenthèses +=1
-        if i == "[":
-            ncrochets +=1
-        if i == "{":
-            naccolades +=1
-        if i == ")":
-            nparenthèses -=1
-        if i == "]":
-            ncrochets -=1
-        if i == "}":
-            naccolades -=1
+    p = Pile()
+    ouvrants = ["(", "[", "{"]
+    fermants = [")", "]", "}"]
 
-    if nparenthèses + ncrochets +  naccolades !=0:
+    for i in x:
+        if i in ouvrants:
+            p.empile(fermants[ouvrants.index(i)])
+
+        if i in fermants:
+            if p.est_vide() or p.depile() != i:
+                return False
+
+    if not p.est_vide():
         return False
     return True
-
-
-
 
 assert test_parenthesage (" (a )( b )((( c )(d )))" )== True
 assert test_parenthesage (" ([b ]){(( c ))[d ]} " )== True
@@ -90,3 +98,34 @@ assert test_parenthesage (" ((a ))}" )== False
 assert test_parenthesage (" [[a ]" )== False
 assert test_parenthesage (" [(a ])" )== False
 assert test_parenthesage (" {((a )})" )== False
+
+#7
+def ranger(p):
+    blanches = Pile()
+    vertes = Pile()
+    while not p.est_vide():
+        el = p.depile
+        if el[0] == "vert":
+            vertes.empile(el)
+        elif el[0] == "blanc":
+            blanches.empile(el)
+
+    while not blanches.est_vide():
+        p.empile(blanches.depile())
+
+    while not vertes.est_vide():
+        p.empile(vertes.depile())
+
+
+
+assiettes = Pile()
+assiettes.empile(("vert", 5))
+assiettes.empile(("vert", 4))
+assiettes.empile(("blanc", 7))
+assiettes.empile(("vert", 1))
+assiettes.empile(("blanc", 9))
+assiettes.empile(("blanc", 7))
+assiettes.empile(("vert", 3))
+print(assiettes)
+ranger(assiettes)
+print(assiettes)
